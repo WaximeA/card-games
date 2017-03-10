ef7e2526c6f3771b12e378515b2460de2ca9f499
MaximeAVELINE <contact@maximeaveline.fr>
2017-03-10 10:01:27 +0100

    config de base de la connexion et de l'auth

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,34 +2,22 @@
 
 namespace AppBundle\Entity;
 
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
- *
- * @ORM\Table(name="user")
- * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
- */
-class User
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user") */
+class User extends BaseUser
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO") */
+    protected $id;
 
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
+    public function __construct() {
+        parent::__construct();
+        // your own logic
     }
 }
-
