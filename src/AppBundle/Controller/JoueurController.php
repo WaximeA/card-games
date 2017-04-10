c414f37c9f957575d1e4935eb72c1bbb0f4082d5
WaximeA <aveline.maxime@gmail.com>
2017-04-10 15:02:21 +0200

    pose de la carte (carte enlevé de la main du joueur)

--- src/AppBundle/Controller/JoueurController.php
+++ src/AppBundle/Controller/JoueurController.php
@@ -60,40 +60,51 @@ class JoueurController extends Controller
         //on mélange les cartes
         shuffle($cartes);
 
+
+        // Recup des cartes de 0 à 7 pour la main du J1
         $t = array();
         for($i = 0; $i<8; $i++)
         {
             $t[] = $cartes[$i]->getId();
         }
-
         $situation->setMainJ1(json_encode($t));
 
+        // Recup des cartes de 8 à 15 pour la main du J2
         $t = array();
         for($i = 8; $i<16; $i++)
         {
             $t[] = $cartes[$i]->getId();
         }
-
         $situation->setMainJ2(json_encode($t));
 
+        // Recup des cartes de 16 au reste pour la pioche
         $t = array();
         for($i = 16; $i<count($cartes); $i++)
         {
             $t[] = $cartes[$i]->getId();
         }
-
         $situation->setPioche(json_encode($t));
 
+        // Actu BDD de situation
         $em = $this->getDoctrine()->getManager();
-
         $em->persist($situation);
         $em->flush();
 
+
+
+        //        // pas besoin de commenter, lis le nom de la varriable
+        //        $creationplateaujoueur = array('ecaille' => array(), 'plume' => array(), 'poil' => array(), 'alien' => array(), 'corne' => array());
+
+        // Création d'une nouvelle partie
         $partie = new Parties();
         $partie->setJoueur1($user);
         $partie->setJoueur2($joueur);
         $partie->setSituation($situation);
+        $partie->setTourde( $user->getId());
+        //        $situation->setCartesPoseesJ1(json_encode($creationplateaujoueur));
+        //        $situation->setCartesPoseesJ2(json_encode($creationplateaujoueur));
 
+        // Actu BDD de partie
         $em->persist($partie);
         $em->flush();
 
@@ -112,17 +123,16 @@ class JoueurController extends Controller
 
         $situation = $id->getSituation();
 
+        // recup de la main du joueur 1 et 2
         $main_joueur1 = $situation->getMainJ1();
         $main_joueur2 = $situation->getMainJ2();
 
+        // Décode des mains dans un plateai
         $plateau['mainJ1'] = json_decode($main_joueur1);
         $plateau['mainJ2'] = json_decode($main_joueur2);
 
         // recup tour de
         $tourde = $id->getTourde();
-
-//        $defausse = $situation->getDefausse();
-//        $defausse['defausse'] = json_decode($defausse);
 
         return $this->render(':joueur:afficherpartie.html.twig', ['cartes' => $cartes, 'partie' => $id, 'user' => $user, 'plateau' => $plateau, 'tourde' => $tourde]);
     }
@@ -142,10 +152,10 @@ class JoueurController extends Controller
         $pioche = $situation->getPioche();
         $piochetab = json_decode($pioche);
 
-//         selectionner le dernier élément du tableau pioche dans dernier
+        // selectionner le dernier élément du tableau pioche dans dernier
         $dernier=array_pop($piochetab);
 
-//         nouvelle pioche sans le $dernier
+        // nouvelle pioche sans le $dernier
         $nouvellepioche = array_diff($piochetab, [$dernier]);
 
         // récuperer l'id des deux joueurs
@@ -155,17 +165,17 @@ class JoueurController extends Controller
         $j2=$id->getJoueur2();
         $idj2=$j2->getId();
 
-        // j actif
+        // joueur actif
         $jactif = $user->getId();
-
 
         // recup tour de
         $tourde = $id->getTourde();
 
+
         if ($tourde == $jactif){
+            // Si je joueur actif = à l'id du joueur 1
             if ($jactif ==  $idj1 ){
-                $main_joueur1 = $situation->getMainJ1();
-                $plateau['mainJ1'] = json_decode($main_joueur1);
+                $plateau['mainJ1'] = json_decode($situation->getMainJ1());
                 $mainj1 = $plateau['mainJ1'];
 
                 //   ajout de dernier dans main du joueur 1
@@ -187,9 +197,10 @@ class JoueurController extends Controller
                 $em->flush();
             }
 
+
+            // Si je joueur actif = à l'id du joueur 2
             if ($jactif ==  $idj2 ){
-                $main_joueur2 = $situation->getMainJ2();
-                $plateau['mainJ2'] = json_decode($main_joueur2);
+                $plateau['mainJ2'] = json_decode($situation->getMainJ2());
                 $mainj2 = $plateau['mainJ2'];
 
                 //   ajout de dernier dans main du joueur 2
@@ -219,6 +230,134 @@ class JoueurController extends Controller
     }
 
 
+    /**
+     * @param Parties $id
+     * @Route("/poser/{id}", name="poser_carte")
+     */
+    public function poserCartePartieAction(Parties $id, Request $request)
+    {
+        $cartes = $this->getDoctrine()->getRepository('AppBundle:Cartes')->getAll();
+        $user = $this->getUser();
+        $situation = $id->getSituation();
+
+        // récuperer l'id des deux joueurs
+        $j1=$id->getJoueur1();
+        $idj1=$j1->getId();
+
+        $j2=$id->getJoueur2();
+        $idj2=$j2->getId();
+
+        // joueur actif
+        $jactif = $user->getId();
+
+        // recup tour de
+        $tourde = $id->getTourde();
+
+        // recup du plateau des mains des joueurs
+        $plateau['mainJ1'] = json_decode($situation->getMainJ1());
+        $plateau['mainJ2'] = json_decode($situation->getMainJ2());
+
+        // recup des cartes posées j1 & j2 : en decode si vide / en ajout tab si plein
+        if (!empty($situation->getCartesPoseesJ1())){
+            $pose_j1 = json_decode($situation->getCartesPoseesJ1());
+        } else {
+        //            $pose_j1 = json_decode($situation->getCartesPoseesJ1());
+        //            array( "1" => []);
+            $pose_j1 = array();
+        }
+        if (!empty($situation->getCartesPoseesJ2())){
+            $pose_j2 = json_decode($situation->getCartesPoseesJ2());
+        } else {
+        //            $pose_j2 = json_decode($situation->getCartesPoseesJ2());
+        //            array( "1" => []);
+            $pose_j2 = array();
+        }
+
+
+        $pose_j1 = array();
+        if ($jactif == $idj1){
+            $main_j1 = $plateau['mainJ1'];
+
+            // recup de la carte select
+            $carteselect = $request->get('carteselect');
+
+            // recup categorie
+            $categoriepose = $request->get('categoriepose');
+
+            // actualiser le plateau
+            array_push( $pose_j1,$carteselect);
+            $em = $this->getDoctrine()->getManager();
+
+            // on refait le tab json
+            $situation->setCartesPoseesJ1(json_encode($pose_j1));
+            $partie=$situation->getId();
+
+            $em->persist($situation);
+            $em->flush();
+
+            $main_j1 = array();
+            $main_j1 = $plateau['mainJ1'];
+            $new_main_j1 = $this->supprimeCarteMain($main_j1,$carteselect);
+
+            $em = $this->getDoctrine()->getManager();
+            $new_main_j1_encode = json_encode($new_main_j1);
+            $situation->setMainJ1($new_main_j1_encode);
+            $em->persist($situation);
+            $em->flush();
+
+        }
+
+        $pose_j2 = array();
+        if ($jactif == $idj2){
+            $main_j2 = $plateau['mainJ2'];
+
+            // recup de la carte select
+            $carteselect = $request->get('carteselect');
+
+            // recup categorie
+            $categoriepose = $request->get('categoriepose');
+
+            // actualiser le plateau
+            array_push( $pose_j2,$carteselect);
+            $em = $this->getDoctrine()->getManager();
+
+            // on refait le tab json
+            $situation->setCartesPoseesJ1(json_encode($pose_j2));
+            $partie=$situation->getId();
+
+            $em->persist($situation);
+            $em->flush();
+
+            $main_j2 = array();
+            $main_j2 = $plateau['mainJ2'];
+            $new_main_j2 = $this->supprimeCarteMain($main_j2,$carteselect);
+
+            $em = $this->getDoctrine()->getManager();
+            $new_main_j2_encode = json_encode($new_main_j2);
+            $situation->setMainJ2($new_main_j2_encode);
+            $em->persist($situation);
+            $em->flush();
+
+        }
+
+
+//        return $this->render(':joueur:poser.html.twig', ['plateau' => $plateau, 'partie' => $id, 'carteselect' => $carteselect]);
+        return $this->redirectToRoute('afficher_partie', ['id' => $id->getId()]);
+    }
+
+    private function supprimeCarteMain($mainj1,$cartecheck)
+    {
+        $t =array();
+        foreach ($mainj1 as $m)
+        {
+            if ($m != $cartecheck)
+            {
+                $t[] = $m;
+            }
+        }
+
+        return $t;
+    }
 
 
 
